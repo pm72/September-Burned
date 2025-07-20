5d0ab59889b56e0780286109e18049a0f9d1555a
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-20 12:23:12 +0400

    pm72

--- enf/main/admin.py
+++ enf/main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . models import Category, Product, ProductImage, Size, productSize
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+  model = ProductImage
+  extra = 1
+
+
+class ProductSizeInline(admin.TabularInline):
+  model = productSize
+  extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'category', 'color', 'price')
+  list_filter = ('category', 'color')
+  search_fields = ('name', 'color', 'description')
+  prepopulated_fields = {'slug': ('name',)}
+  inlines = (ProductImageInline, ProductSizeInline)
