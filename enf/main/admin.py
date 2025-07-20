4be6afcd60ef93d2cdcd2f652303f5b9132b30c5
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-20 15:52:06 +0400

    pm72

--- enf/main/admin.py
+++ enf/main/admin.py
@@ -18,3 +18,17 @@ class ProductAdmin(admin.ModelAdmin):
   search_fields = ('name', 'color', 'description')
   prepopulated_fields = {'slug': ('name',)}
   inlines = (ProductImageInline, ProductSizeInline)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'slug')
+  prepopulated_fields = {'slug': ('name',)}
+
+
+class SizeAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+
+
+admin.site.register(Size, SizeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
