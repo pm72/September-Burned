0a02a79b4549234bcf7d078c38dee34068d0daee
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-26 17:11:40 +0400

    pm72

--- enf/main/admin.py
+++ enf/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Category, Product, ProductImage, Size, productSize
+from . models import Category, Product, ProductImage, Size, ProductSize
 
 
 class ProductImageInline(admin.TabularInline):
@@ -8,7 +8,7 @@ class ProductImageInline(admin.TabularInline):
 
 
 class ProductSizeInline(admin.TabularInline):
-  model = productSize
+  model = ProductSize
   extra = 1
 
 
